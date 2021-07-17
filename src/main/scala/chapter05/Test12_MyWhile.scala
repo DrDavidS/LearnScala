6f1906d826c90c5774162b4f4efd010a946a9463
DrDavidS <312648004@qq.com>
2021-07-18 02:26:33 +0800

    实现一个MyWhile,增加尾递归提示

--- src/main/scala/chapter05/Test12_MyWhile.scala
+++ src/main/scala/chapter05/Test12_MyWhile.scala
@@ -1,5 +1,7 @@
 package chapter05
 
+import scala.annotation.tailrec
+
 object Test12_MyWhile {
   def main(args: Array[String]): Unit = {
     var n = 10
@@ -12,6 +14,7 @@ object Test12_MyWhile {
 
     // 2. 用闭包实现一个函数，将代码块作为参数传入，递归调用
     println("===========闭 包 实 现============================")
+
     def myWhile(condition: => Boolean): (=> Unit) => Unit = {
       // 内层函数需要递归调用
       def doLoop(op: => Unit): Unit = {
@@ -20,17 +23,19 @@ object Test12_MyWhile {
           myWhile(condition)(op)
         }
       }
+
       doLoop _
     }
 
     n = 10
-    myWhile(n >= 1){
+    myWhile(n >= 1) {
       println(n)
       n -= 1
     }
 
     // 3. 用匿名函数实现
     println("=============匿 名 函 数 实 现==========================")
+
     def myWhile2(condition: => Boolean): (=> Unit) => Unit = {
       // 内层函数需要递归调用
       op => {
@@ -40,8 +45,9 @@ object Test12_MyWhile {
         }
       }
     }
+
     n = 10
-    myWhile2(n >= 1){
+    myWhile2(n >= 1) {
       println(n)
       n -= 1
     }
@@ -50,15 +56,18 @@ object Test12_MyWhile {
     // 4. 柯里化实现
     // 这个很简洁了
     println("===============柯 里 化========================")
-    def myWhile3(condition: => Boolean)(op: =>Unit): Unit = {
+
+    @tailrec
+    def myWhile3(condition: => Boolean)(op: => Unit): Unit = {
       // 内层函数需要递归调用
-        if (condition) {
-          op
-          myWhile3(condition)(op)
-        }
+      if (condition) {
+        op
+        myWhile3(condition)(op)
       }
+    }
+
     n = 10
-    myWhile3(n >= 1){
+    myWhile3(n >= 1) {
       println(n)
       n -= 1
     }
