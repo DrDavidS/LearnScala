9aa52afcf0bc0cbf52d878b18bbd562b6d2c6fbb
DrDavidS <312648004@qq.com>
2021-07-21 22:34:02 +0800

    不可变数组

--- src/main/scala/chapter07/Test01_ImmutableArray.scala
+++ src/main/scala/chapter07/Test01_ImmutableArray.scala
@@ -6,7 +6,7 @@ object Test01_ImmutableArray {
     val arr: Array[Int] = new Array[Int](5) // 默认元素是0
 
     // 另一种创建的方式
-    val arr2 = Array(12, 37, 42, 58, 97) // apply方法可以省略
+    val arr2: Array[Int] = Array(12, 37, 42, 58, 97) // apply方法可以省略
 
     // 2. 访问元素
     println("============= 访问元素 =============")
@@ -37,7 +37,7 @@ object Test01_ImmutableArray {
 
     // 3.3 迭代器的使用
     println("============= 迭代器 =============")
-    val iter = arr2.iterator
+    val iter: Iterator[Int] = arr2.iterator
 
     while (iter.hasNext) {
       println(iter.next)
@@ -49,5 +49,19 @@ object Test01_ImmutableArray {
     arr.foreach(println)
 
     println(arr2.mkString("--")) // 所有元素双横线连接
+
+    // 4. 添加元素
+    println("============= 添加元素 =============")
+    val newArr: Array[Int] = arr2.:+(73) // 往最后面添加
+    println(arr2.mkString("--"))
+    println(newArr.mkString("--"))
+
+    val newArr2: Array[Int] = newArr.+:(30)
+    println(newArr2.mkString("--"))
+
+    // 简单点的写法
+    val newArr3: Array[Int] = newArr2 :+ 15 // 后面加15
+    val newArr4: Array[Int] = 19 +: 29 +: newArr3 :+ 26 :+ 73
+    println(newArr4.mkString("--"))
   }
 }
